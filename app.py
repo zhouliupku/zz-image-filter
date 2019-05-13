2e988112ac088e67ffed6b77d2b8b11f0680a09f
vtsen <zhenhumit@gmail.com>
2019-05-12 21:28:18 -0400

    improve hierarchy of filters
    improve hierarchy of filters

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 from PIL import Image, ImageTk
 
 from .common import config
-from .filters.point_filters import polarize_filter
+from .filters.point_filters import PolarizeFilter
 
 class App:
     def __init__(self, window, window_title):
@@ -22,19 +22,24 @@ class App:
         root_path = os.path.dirname(os.path.abspath(__file__))
         img_path = os.path.join(root_path, "resources", "image")
         
+        # Initialize app window
         self.window = window
         self.window.title(window_title)
         self.window.geometry("600x350")
         self.window.wm_iconbitmap(os.path.join(img_path, "zz.ico"))
         
+        # Title object
         title = tk.Label(text="Filter Project", font=config.TITLE_FONT)
         title.grid(row=0, column=0, columnspan=3)
 
+        # Load original image
+        #TODO: add load functionality
         self.orig_img = cv2.imread(os.path.join(img_path, "orig", "pku.jpg"),
                               cv2.IMREAD_GRAYSCALE)
         self.out_img = self.orig_img
         print(self.orig_img)
         
+        # Show original image
         height, width = self.orig_img.shape
         self.orig_canvas = tk.Canvas(window, width=width, height=height)
         self.orig_canvas.grid(row=1, column=0)
@@ -44,6 +49,7 @@ class App:
                                       image=self.orig_photo,
                                       anchor=tk.NW)
         
+        # Show output image, initialized as a copy of original image
         self.out_canvas = tk.Canvas(window, width=width, height=height)
         self.out_canvas.grid(row=1, column=2)
         self.out_photo = ImageTk.PhotoImage(master=self.out_canvas,
@@ -52,8 +58,10 @@ class App:
                                           image=self.out_photo,
                                           anchor=tk.NW)
         
+        # Button to apply filter and update output image
+        f = PolarizeFilter()
         apply_filter_button = tk.Button(text="Apply", bg="white",
-                                        command=lambda:self.apply(polarize_filter))
+                                        command=lambda:self.apply(f))
         apply_filter_button.grid(row=1, column=1)
         
         # Saving
@@ -64,17 +72,19 @@ class App:
         # Start main loop
         self.window.mainloop()
         
-    def apply(self, point_filter):
+        
+    def apply(self, img_filter):
         """
-        Apply filter pointwise to self.orig_img, set and display self.out_img
-        point_filter: the filter to be applied
+        Apply filter to self.orig_img, set and display self.out_img
+        img_filter: the filter to be applied, an instance of filters.ImageFilter
         """
         #TODO: ensure value between 0 and 255
-        self.out_img = point_filter(self.orig_img)
+        self.out_img = img_filter.apply(self.orig_img)
         print(self.out_img)
         self.out_photo = ImageTk.PhotoImage(master=self.out_canvas,
                                             image=Image.fromarray(self.out_img))
         self.out_canvas.itemconfig(self.out_img_on_canvas, image = self.out_photo)
+        
         
     def save(self, img):
         """
@@ -87,6 +97,7 @@ class App:
         save_path = os.path.join(root_path, "resources", "image", "out")
         cv2.imwrite(os.path.join(save_path, filename), img)
         self.after_save(os.path.join(save_path, filename))
+        
         
     def after_save(self, filename):
         """

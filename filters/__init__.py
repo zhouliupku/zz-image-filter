7450cc43d5024b02e626ca999d53fd5ef91e4841
vtsen <zhenhumit@gmail.com>
2019-05-16 21:13:19 -0400

    support local filter
    this version now supports local convolutional filter with a simple example of average blur filter

--- filters/__init__.py
+++ filters/__init__.py
@@ -5,7 +5,8 @@ Created on Sun May 12 18:29:06 2019
 @author: Zhen
 """
 
-from . import point_filters
+from . import point_filters, local_filters
 
 filter_dict = {"polar": point_filters.PolarizeFilter(),
-               "whiten": point_filters.WhitenFilter()}
+               "whiten": point_filters.WhitenFilter(),
+               "blur": local_filters.AverageBlurFilter()}
